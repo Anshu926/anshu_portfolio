d7e487c54f3425432d247a73f46eafc81a6f4434
Lord-Anshu <bongadeanshu@gmail.com>
2026-04-19 14:12:26 +0530

    new commit 1

--- Frontend/src/Typewriter.jsx
+++ Frontend/src/Typewriter.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 
 const words = [
-  "Full Stack Web Development.",
-  "Mobile Application Development.",
-  "AI / ML Development."
+  "Full Stack Developer.",
+  "Mobile App Developer.",
+  "AI / ML Enthusiast.",
+  "Prompt Engineering Enthusiast.",
 ];
 
 function Typewriter() {
